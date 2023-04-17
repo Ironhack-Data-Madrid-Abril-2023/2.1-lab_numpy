01cf82cc2e3c88613cbd3609c4c668fd96cfdd79
Giotto Millan <giottomillan@gmail.com>
2023-04-17 17:05:20 +0200

    [Giotto](2.1-lab_numpy)

--- main.py
+++ main.py
@@ -1,68 +1,88 @@
 #1. Import the NUMPY package under the name np.
+import numpy as np
 
 
 
 #2. Print the NUMPY version and the configuration.
-
+print("NumPy version:", np.__version__)
+print("\nNumPy configuration:")
+np.show_config()
 
 
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
-
+a = np.random.random((2,3,5))
 
 
 #4. Print a.
 
-
+print(a)
 
 #5. Create a 5x2x3 3-dimensional array with all values equaling 1.
 #Assign the array to variable "b"
 
-
+b = np.random.random ((5,2,3))
 
 #6. Print b.
 
+print(b)
 
 
 #7. Do a and b have the same size? How do you prove that in Python code?
 
-
+print(a.size == b.size)
 
 
 #8. Are you able to add a and b? Why or why not?
-
-
+print(a.shape)
+#print(a + b)
+#No se pueden sumar porque tienen forma diferente, se tendría que trasponer para hacer la operación.
 
 #9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
 
+c = b.reshape(2,3,5)
+
+print(c.shape)
 
 
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
 
-
+d = a + c
+print(d)
 
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
 
+print("Este es ====> a", a)
 
+print("Este es ====> d", d)
 
+# "d" es una versión escalada de "a" con valores más grandes en comparación con "a"
 
 #12. Multiply a and c. Assign the result to e.
+
+e = a * c
 
 
 
 #13. Does e equal to a? Why or why not?
 
-
-
+# e no es igual a a. Aunque ambas matrices tienen la misma forma, los valores de sus elementos son diferentes.
+print (e == a)
 
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
 
+d_max = np.max(d)
+d_min = np.min(d)
+d_mean = np.mean(d)
 
-
+print("Max---------->", d_max)
+print("Min---------->", d_min)
+print("Mean--------->", d_mean)
 
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
 
-
+f = np.empty_like(d)
+print("Este es f----------->", f)
 
 
 """
