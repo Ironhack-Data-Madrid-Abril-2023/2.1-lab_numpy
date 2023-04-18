18dcb6c99b0264fe2c99c6fa91f3ff952093d773
Linxx333 <nachoromerogsd@gmail.com>
2023-04-18 10:23:52 +0200

    Juanito me ayudo

--- main.py
+++ main.py
@@ -1,68 +1,79 @@
 #1. Import the NUMPY package under the name np.
 
-
+import numpy as np
 
 #2. Print the NUMPY version and the configuration.
 
-
+print("NumPy version:", np.__version__)
+print("\nNumPy configuration:\n", np.show_config())
 
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
 
-
+a = np.random.random((2,3,5))
 
 #4. Print a.
 
-
+print(a)
 
 #5. Create a 5x2x3 3-dimensional array with all values equaling 1.
 #Assign the array to variable "b"
 
-
+b = np.ones((5, 2, 3))
 
 #6. Print b.
 
-
+print(b)
 
 #7. Do a and b have the same size? How do you prove that in Python code?
 
-
+a.size == b.size
 
 
 #8. Are you able to add a and b? Why or why not?
 
-
+#I cant as they dont have the exact same dimensions
 
 #9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
 
-
+c = np.reshape(b, (2, 3, 5))
 
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
 
-
+d = a + c
 
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
 
-
-
+print(a) 
+print(d)
+#The diference between their values is b values as d is a + b
 
 #12. Multiply a and c. Assign the result to e.
 
-
+e = a * c
 
 #13. Does e equal to a? Why or why not?
 
 
-
+#Yes as a was multiplied by a unitary matrix
 
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
 
+d_max = d.max()
+print(d_max)
+
+d_min = d.min()
+print(d_min)
+
+d_mean=d.mean()
+print(d_mean)
 
 
 
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
 
-
+f = np.empty((2, 3, 5))
+print(f)
 
 
 """
@@ -74,6 +85,12 @@ Assign 100 to the corresponding value(s) in f for d_max in d.
 In the end, f should have only the following values: 0, 25, 50, 75, and 100.
 Note: you don't have to use Numpy in this question.
 """
+
+f[(d_mean > d) & (d > d_min)] = 25
+f[(d_max > d) & (d > d_mean)] = 75
+f[(d == d_mean)] = 50
+f[(d == d_min)] = 0
+f[(d == d_max)] = 100
 
 
 
@@ -98,7 +115,8 @@ array([[[ 75.,  75.,  75.,  25.,  75.],
         [ 75.,  75.,  75.,  75.,  75.],
         [ 25.,  75.,   0.,  75.,  75.]]])
 """
-
+print (d)
+print(f)
 
 """
 #18. Bonus question: instead of using numbers (i.e. 0, 25, 50, 75, and 100), how to use string values 
@@ -112,3 +130,13 @@ array([[[ 'D',  'D',  'D',  'B',  'D'],
         [ 'B',  'D',   'A',  'D', 'D']]])
 Again, you don't need Numpy in this question.
 """
+
+f_str = f.astype(str)
+
+f_str[(d_mean > d) & (d > d_min)] = 'B'
+f_str[(d_max > d) & (d > d_mean)] = 'D'
+f_str[(d == d_mean)] = 'C'
+f_str[(d == d_min)] = 'A'
+f_str[(d == d_max)] = 'E'
+
+f_str
